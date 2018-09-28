944626ef269b3fe7407cfbbb570bdb5d65b8536d
Concross <cacrossley22@gmail.com>
2018-09-28 13:11:37 -0700

    file structure

---
+++ server/src/api/api.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+router.post(() => {
+
+});
+
+router.get(() => {
+
+});
+
+router.put(() => {
+
+});
+
+router.delete(() => {
+
+});
+
+export default router;
