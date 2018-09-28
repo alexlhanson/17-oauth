458613c8be27948bebb420a13916b357cd5c1680
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-28 15:03:24 -0700

    competed webserver

--- web-server/index.js
+++ web-server/index.js
@@ -4,4 +4,11 @@ require('dotenv').config();
 
 require('babel-register');
 
-require('./src/app').start(process.env.PORT);
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.static('./public'));
+app.listen(process.env.PORT, () => console.log(`listening on port: ${process.env.PORT}`));
+ 
