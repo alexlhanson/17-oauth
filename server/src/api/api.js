a1682bcb2a0d3074f90f77e26eecb82e9da81b19
Sharon Miller <sharon@sharonmillercreative.com>
2018-10-01 16:45:38 -0700

    create signup form to save users to mongodb

--- server/src/api/api.js
+++ server/src/api/api.js
@@ -3,9 +3,14 @@
 import express from 'express';
 const router = express.Router();
 import authorize from '../auth/lib/oauth.js';
+import User from '../auth/model.js';
 
-router.post(() => {
-
+router.post('/register', (req, res, next) => {
+  console.log('Im posting');
+  let user = new User(req.body);
+  user.save()
+    .then(user => res.send(user.generateToken()))
+    .catch(next);
 });
 
 router.get('/oauth', (req, res, next) => {
@@ -18,7 +23,7 @@ router.get('/oauth', (req, res, next) => {
       res.redirect(process.env.REDIRECT_CLIENT_URI);
 
     })
-    
+
     .catch(error => error);
 
 });
@@ -29,6 +34,10 @@ router.put(() => {
 
 router.delete(() => {
 
+});
+
+router.all('*', (req, res, next) => {
+  res.status(404).send('Bad request').end();
 });
 
 export default router;
--- server/src/auth/model.js
+++ server/src/auth/model.js
@@ -8,9 +8,32 @@ import mongoose from 'mongoose';
 
 let Schema = mongoose.Schema;
 
+
 const userSchema = Schema({
   username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
+
+userSchema.pre('save', function (next) {
+  console.log('what is this: ', this);
+  try {
+    bcrypt.hash(this.password, 2)
+      .then(hashedPassword => {
+        console.log('the hashedPassword: ', hashedPassword);
+
+        this.password = hashedPassword;
+        console.log('this password is???:', this.password);
+        next();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } catch(e){
+    console.log(e);
+  }
+  console.log('my random console log!!!');
+});
+
 
 userSchema.statics.createFromOAuth = function (githubUser) {
   console.log('creating user from github user');
@@ -21,7 +44,7 @@ userSchema.statics.createFromOAuth = function (githubUser) {
   console.log('im the githubUser:', githubUser);
   return this.findOne({ username: githubUser.login })
     .then(user => {
-      if (!user) { throw new Error('User not found');}
+      if (!user) { throw new Error('User not found'); }
       console.log('welcome back');
       return user;
     })
@@ -35,7 +58,7 @@ userSchema.statics.createFromOAuth = function (githubUser) {
 };
 
 userSchema.methods.generateToken = function () {
-  return jwt.sign({id:this._id}, process.env.SECRET);
+  return jwt.sign({ id: this._id }, process.env.SECRET);
 };
 
 export default mongoose.model('users', userSchema);
