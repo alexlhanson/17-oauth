50eb8201f6a70382bef60fdfe0595922e7e4b3ce
alexlhanson <alexlhanson@gmail.com>
2018-10-10 20:46:17 -0700

    Init commit on new machine with envs

--- web-server/index.js
+++ web-server/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-require('dotenv').config();
-
 require('babel-register');
+require('dotenv').config();
 
 const express = require('express');
 
@@ -11,4 +10,3 @@ const app = express();
 
 app.use(express.static('./public'));
 app.listen(process.env.PORT, () => console.log(`listening on port: ${process.env.PORT}`));
- 
